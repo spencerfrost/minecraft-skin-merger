e22c9e7d1e058ba5f47957ffd28584217f751bea
Spencer <s.s.frost@gmail.com>
2024-10-28 18:59:36 -0400

    update default port

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,21 +1,17 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Constants
-const PROD_DOMAIN = "https://mcskinmerger.mrspinn.ca";
-const DEFAULT_PORT = 3002;
-
-// Environment
 const isDev = process.env.NODE_ENV !== "production";
+
+const PROD_DOMAIN = "https://mcskinmerger.mrspinn.ca";
+const DEFAULT_PORT = isDev ? 3020 : 3220;
 const PORT = process.env.PORT || DEFAULT_PORT;
 
-// File paths
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PUBLIC_DIR = isDev
-  ? path.join(__dirname, "..", "..", "public") // Development: [project_root]/public
+  ? path.join(__dirname, "..", "..", "public")
   : path.join(__dirname, "..", "public");
 
-// CORS
 const corsOptions = {
   origin: isDev ? /^http:\/\/localhost:\d+$/ : PROD_DOMAIN,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
