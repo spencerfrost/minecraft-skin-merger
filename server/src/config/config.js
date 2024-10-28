b3dd8f9dac72d9f69eefd6f62d26e3460cd9d11d
Spencer <s.s.frost@gmail.com>
2024-10-28 19:14:23 -0400

    Change relative public dir path in config file

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -8,9 +8,7 @@ const DEFAULT_PORT = isDev ? 3020 : 3220;
 const PORT = process.env.PORT || DEFAULT_PORT;
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const PUBLIC_DIR = isDev
-  ? path.join(__dirname, "..", "..", "public")
-  : path.join(__dirname, "..", "public");
+const PUBLIC_DIR = path.join(__dirname, "..", "..", "public");
 
 const corsOptions = {
   origin: isDev ? /^http:\/\/localhost:\d+$/ : PROD_DOMAIN,
