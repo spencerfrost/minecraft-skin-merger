e2e161ef6012630d642cac85a9776a2328fee28e
Spencer <s.s.frost@gmail.com>
2024-10-28 18:40:54 -0400

    Refactor config file to use relative path for script and improve CORS handling

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,23 +1,25 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 // Constants
-const PROD_DOMAIN = 'https://mcskinmerger.mrspinn.ca';
+const PROD_DOMAIN = "https://mcskinmerger.mrspinn.ca";
 const DEFAULT_PORT = 3002;
 
 // Environment
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || DEFAULT_PORT;
 
 // File paths
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const PUBLIC_DIR = path.join(__dirname, '..', '..', 'public');
+const PUBLIC_DIR = isDev
+  ? path.join(__dirname, "..", "..", "public") // Development: [project_root]/public
+  : path.join(__dirname, "..", "public");
 
 // CORS
 const corsOptions = {
   origin: isDev ? /^http:\/\/localhost:\d+$/ : PROD_DOMAIN,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 export default {
@@ -25,5 +27,5 @@ export default {
   isDev,
   DOMAIN: isDev ? `http://localhost:${PORT}` : PROD_DOMAIN,
   PUBLIC_DIR,
-  corsOptions
+  corsOptions,
 };
